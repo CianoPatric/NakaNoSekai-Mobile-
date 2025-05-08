b1e365d3a232b80d1d352966cef7708074e3d426
DESKTOP-CDOD8P1\nikit <nikitka.kopylov.2018@bk.ru>
2025-05-08 17:09:49 +0400

    a

--- Assets/Scripts/DI/DIEntry.cs
+++ Assets/Scripts/DI/DIEntry.cs
@@ -1,80 +1,72 @@
 using System;
-
-namespace BaCon
+public abstract class DIEntry : IDisposable 
 {
-    public abstract class DIEntry : IDisposable
+    protected DIContainer Container { get; } 
+    protected bool IsSingleton { get; set; }
+
+    protected DIEntry() { }
+
+    protected DIEntry(DIContainer container)
     {
-        protected DIContainer Container { get; }
-        protected bool IsSingleton { get; set; }
-
-        protected DIEntry() { }
-        
-        protected DIEntry(DIContainer container)
-        {
-            Container = container;
-        }
-
-        public T Resolve<T>()
-        {
-            return ((DIEntry<T>)this).Resolve();
-        }
-
-        public DIEntry AsSingle()
-        {
-            IsSingleton = true;
-
-            return this;
-        }
-
-        public abstract void Dispose();
+        Container = container; 
     }
-    
-    public class DIEntry<T> : DIEntry
+
+    public T Resolve<T>()
     {
-        private Func<DIContainer, T> Factory { get; }
-        private T _instance;
-        private IDisposable _disposableInstance;
+        return ((DIEntry<T>)this).Resolve(); 
+    }
+
+    public DIEntry AsSingle()
+    { 
+        IsSingleton = true; 
+        return this;
+    }
         
-        public DIEntry(DIContainer container, Func<DIContainer, T> factory) : base(container)
-        {
-            Factory = factory;
+    public abstract void Dispose();
+}
+    
+public class DIEntry<T> : DIEntry
+{
+    private Func<DIContainer, T> Factory { get; }
+    private T _instance;
+    private IDisposable _disposableInstance;
+    public DIEntry(DIContainer container, Func<DIContainer, T> factory) : base(container)
+    {
+        Factory = factory;
+    }
+    public DIEntry(T value)
+    {
+        _instance = value;
+        
+        if (_instance is IDisposable disposableInstance) 
+        { 
+            _disposableInstance = disposableInstance;
         }
+        IsSingleton = true;
+    }
 
-        public DIEntry(T value)
-        {
-            _instance = value;
-
-            if (_instance is IDisposable disposableInstance)
-            {
-                _disposableInstance = disposableInstance;
+    public T Resolve() 
+    { 
+        if (IsSingleton) 
+        { 
+            if (_instance == null) 
+            { 
+                _instance = Factory(Container);
+                
+                if (_instance is IDisposable disposableInstance) 
+                { 
+                    _disposableInstance = disposableInstance;
+                }
             }
             
-            IsSingleton = true;
+            return _instance;
         }
 
-        public T Resolve()
-        {
-            if (IsSingleton)
-            {
-                if (_instance == null)
-                {
-                    _instance = Factory(Container);
-                    
-                    if (_instance is IDisposable disposableInstance)
-                    {
-                        _disposableInstance = disposableInstance;
-                    }
-                }
+        return Factory(Container);
+    }
 
-                return _instance;
-            }
-
-            return Factory(Container);
-        }
-
-        public override void Dispose()
-        {
-            _disposableInstance?.Dispose();
-        }
+    public override void Dispose() 
+    { 
+        _disposableInstance?.Dispose();
     }
 }
