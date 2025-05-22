f50615bb39fc80fc45e7aa6dc4585aab7ec396f4
DESKTOP-CDOD8P1\nikit <nikitka.kopylov.2018@bk.ru>
2025-05-22 14:14:32 +0400

    Supabase

---
+++ Assets/Scripts/ClientInfo.cs
@@ -0,0 +1,8 @@
+namespace DefaultNamespace
+{
+    public static class ClientInfo
+    {
+        public const string CLIENT_URL = "https://xqgzesomfqogrgvwajip.supabase.co";
+        public const string CLIENT_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxZ3plc29tZnFvZ3JndndhamlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0NTc1MDQsImV4cCI6MjA0ODAzMzUwNH0.k_KVetv9AbFkpiAFIjHPtOmxRv-JTAj-aXCnaHJWeAU";
+    }
+}
