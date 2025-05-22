65ca07d1d45187bfff4d4800b38c14e0b7a139d0
DESKTOP-CDOD8P1\nikit <nikitka.kopylov.2018@bk.ru>
2025-05-22 14:41:10 +0400

    Корректировка

---
+++ Assets/Scripts/SupabaseScripts/Connection.cs
@@ -0,0 +1,13 @@
+using DefaultNamespace;
+using Supabase;
+using Unity.VisualScripting;
+
+namespace SupabaseScripts
+{
+    public class Connection
+    {
+        public static string url = ClientInfo.CLIENT_URL;
+        public static string key = ClientInfo.CLIENT_KEY;
+        
+    }
+}
