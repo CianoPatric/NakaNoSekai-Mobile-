94420c59aae6d4ed0ee21d54e7f89b22ae21c852
DESKTOP-CDOD8P1\nikit <nikitka.kopylov.2018@bk.ru>
2025-05-25 22:57:12 +0400

    Регистрация

--- Assets/Scripts/SupabaseScripts/Connection.cs
+++ Assets/Scripts/SupabaseScripts/Connection.cs
@@ -20,8 +20,13 @@ namespace SupabaseScripts
         
         public GameObject AULogin;
         public GameObject AUPassword;
+        
         public GameObject RELogin;
         public GameObject REPassword;
+        public GameObject REPasswordRepit;
+        
+        public GameObject AuthPrefab;
+        public GameObject RegPrefab;
 
         public GameObject ParkName;
         private string nameU;
@@ -45,7 +50,7 @@ namespace SupabaseScripts
         {
             try
             {
-                UserDb userDb = new UserDb
+                UserDb userDb = new()
                 {
                     nickname = AULogin.GetComponent<TextMeshProUGUI>().text,
                     password = AUPassword.GetComponent<TextMeshProUGUI>().text
@@ -55,11 +60,42 @@ namespace SupabaseScripts
                 {
                     nameU = AULogin.GetComponent<TextMeshProUGUI>().text;
                     passwordU = AUPassword.GetComponent<TextMeshProUGUI>().text;
+                    AuthPrefab.SetActive(false);
                 }
             }
             catch (Exception e)
             {
-                Debug.Log("Ой, "+ e.Message);
+                Debug.Log("Ой, " + e.Message);
+            }
+        }
+        public async void Registr()
+        {
+            try
+            {
+                var RepitPass = REPasswordRepit.GetComponent<TextMeshProUGUI>().text;
+                var Pass = REPassword.GetComponent<TextMeshProUGUI>().text;
+                if(RepitPass == Pass)
+                {
+                    UserDb userDb = new()
+                    {
+                        nickname = RELogin.GetComponent<TextMeshProUGUI>().text,
+                        password = Pass
+                    };
+                    var reg = await UserDb.RegisterUserAsync(supabase, userDb);
+                    if(reg)
+                    {
+                        await UserDb.AuthenticateUserAsync(supabase, userDb);
+                        RegPrefab.SetActive(false);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Проверьте соответсвия пароля и его подтверждения");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Ой, " + e.Message);
             }
         }
     }
